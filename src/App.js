fc0c20983a195d32be441a6b93a8b308e1c9a1b3
MirabdullaYuldashev <mirabdullayuldashev@gmail.com>
2023-04-01 13:28:30 +0600

    Home work 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Это мое первое дз по React!</h1>
-    </div>
-  );
+import React from "react";
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+    return nextState.count !== this.state.count;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 5 });
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleClick}>Нажми, чтобы добавить 5</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyComponent
